912cfb372e1a3ead5267f6d2eb4703ac7dd4b0dc
tuesvon <tuesvon@yandex.ru>
2024-04-06 14:57:02 +0300

    апдейты

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from selenium.webdriver.chrome.options import Options as OptGC
 from selenium.webdriver.firefox.options import Options as OptFF
 
 def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default=None,
+    parser.addoption('--browser_name', action='store', default="chrome",
                      help="Choose browser: chrome or firefox")
     parser.addoption('--language', action='store', default="en", help="Choose language: ru, en, es, fr...etc...")
 
