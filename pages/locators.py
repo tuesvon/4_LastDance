f4112e27e613c99e6f901285f21cda756df1dad3
tuesvon <tuesvon@yandex.ru>
2024-04-08 18:29:40 +0300

    апдейты

--- pages/locators.py
+++ pages/locators.py
@@ -2,3 +2,4 @@ from selenium.webdriver.common.by import By
 
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+    REG_LINK = (By.CSS_SELECTOR, "#registration_link")
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,13 @@
 from .base_page import BasePage
+from .locators import MainPageLocators
 from selenium.webdriver.common.by import By
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
+        login_link = self.browser.find_element(*MainPageLocators.REG_LINK)
         login_link.click()
 
     def should_be_login_link(self):
-        assert self.browser.find_element(By.CSS_SELECTOR, "#login_link"), "login link is not presented"
+        assert self.browser.find_element(*MainPageLocators.REG_LINK), "login link is not presented"
 
