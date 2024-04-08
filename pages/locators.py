9918bb5d144ff258d7b5f067699c52525817dff5
tuesvon <tuesvon@yandex.ru>
2024-04-08 20:26:43 +0300

    апдейты

--- pages/locators.py
+++ pages/locators.py
@@ -3,3 +3,5 @@ from selenium.webdriver.common.by import By
 class MainPageLocators():
     LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
     REG_LINK = (By.CSS_SELECTOR, "#registration_link")
+
+class LoginPageLocators(): select_d
