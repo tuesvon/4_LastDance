d7d3abc8cee7aa6e6594628ada634db00b43e4fa
tuesvon <tuesvon@yandex.ru>
2024-04-10 17:48:06 +0300

    добавил xfail

--- test_page_elements.py
+++ test_page_elements.py
@@ -1,11 +1,15 @@
 from pages.product_page import ProductPage
+import pytest
 
+
+@pytest.mark.xfail
 def test_guest_cant_see_success_message_after_adding_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.open()
     page.should_add_item_to_the_basket()
     page.should_not_be_success_message()
+
 
 def test_guest_cant_see_success_message(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
@@ -14,11 +18,10 @@ def test_guest_cant_see_success_message(browser):
     page.should_not_be_success_message()
 
 
+@pytest.mark.xfail
 def test_message_disappeared_after_adding_product_to_basket(browser):
     link = 'http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/'
     page = ProductPage(browser, link)
     page.open()
     page.should_add_item_to_the_basket()
     page.success_message_should_disappear()
-
-
