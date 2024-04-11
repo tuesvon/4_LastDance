7fe85a34336120cd43f5473687b06dcbbadcf811
tuesvon <tuesvon@yandex.ru>
2024-04-11 18:58:05 +0300

    корректировка 4.3.11

--- test_product_page.py
+++ test_product_page.py
@@ -17,7 +17,8 @@ class TestUserAddToBasketFromProductPage():
         login_page.should_be_authorized_user()
 
     def test_user_cant_see_product_in_basket_opened_from_product_page(self, browser):
-        page = ProductPage(browser, self.link)
+        link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
+        page = ProductPage(browser, link)
         page.open()
         page.items_in_basket()
         basket_page = BasketPage(browser, self.link)
@@ -25,7 +26,8 @@ class TestUserAddToBasketFromProductPage():
         basket_page.your_basket_is_empty_text()
 
     def test_user_can_add_product_to_basket(self, browser):
-        page = ProductPage(browser, self.link)
+        link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+        page = ProductPage(browser, link)
         page.open()
         page.should_add_item_to_the_basket()
         page.solve_quiz_get_code()
