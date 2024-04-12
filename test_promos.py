44433db3f4dc492ee16ac6aa93d435d34902b012
tuesvon <tuesvon@yandex.ru>
2024-04-12 11:35:51 +0300

    ещё небольшие правки для ревьюеров

--- test_promos.py
+++ test_promos.py
@@ -2,6 +2,7 @@ from pages.product_page import ProductPage
 import pytest
 
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('offer', ["0", "1", "2", "3", "4", "5", "6",
                                    pytest.param("7", marks=pytest.mark.xfail), "8", "9"])
 def test_guest_can_add_product_to_basket(browser, offer):
