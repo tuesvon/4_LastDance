d2f8c05f320020d635fb30c2d6eecc44516cc12b
tuesvon <tuesvon@yandex.ru>
2024-04-12 11:56:43 +0300

    ещё небольшие правки для ревьюеров

--- test_product_page.py
+++ test_product_page.py
@@ -50,12 +50,14 @@ def test_guest_should_see_login_link_on_product_page(browser):
     page.open()
     page.should_be_login_link()
 
+
 @pytest.mark.need_review
 def test_guest_can_go_to_login_page_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"
     page = ProductPage(browser, link)
     page.open()
     page.go_to_login_page()
+
 
 @pytest.mark.need_review
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
